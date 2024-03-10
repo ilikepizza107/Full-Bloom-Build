afda91dc610196f2e70c7da8d669aaa2bab00381
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2024-03-10 13:41:54 -0500

    Update to 2.5.2

--- Build/Project+/Source/Community/ItemEx.asm
+++ Build/Project+/Source/Community/ItemEx.asm
@@ -1,6 +1,6 @@
 
 ################################################
-ItemEx Clone Engine v2.2 [Sammi Husky, Kapedani]
+ItemEx Clone Engine v2.3 [Sammi Husky, Kapedani]
 ################################################
 # Stages can override items
 # Character specific items
@@ -1142,6 +1142,10 @@ HOOK @ $809b1324    # itManager::createBaseItem
     mr r3, r15                              # \
     mr r4, r18                              # |
     li r5, 0                                # | Try item variant 0 instead
+    cmplwi r19, 0xFFFF                      # |
+    ble+ notFighterItem                     # | (or variant & 0xFFFF00FF if it's a fighter item)
+    rlwinm r5,r19,0,24,15                   # |
+notFighterItem:                             # |
     li r6, 1                                # |
     %call (itManager__checkCreatableItem)   # |
     cmpwi r3, 0                             # /
@@ -1152,30 +1156,41 @@ HOOK @ $809b137c    # itManager::createBaseItem
     lbz r12, 0x8(r1)    # \
     cmpwi r12, 0x1      # | If desired item variant isn't creatable
     beq+ %end%          # |
-    li r25, 0x0         # /
+    li r25, 0x0         # use variant 0
+    cmplwi r19, 0xFFFF         # \
+    ble+ %end%                 # | variant & 0xFFFF00FF if it's a fighter item)-
+    rlwinm r25,r19,0,24,15     # /
 }
 HOOK @ $809b1540    # itManager::createBaseItem
 {
     andc r28, r19, r0   # Original operation
     lbz r12, 0x8(r1)    # \
-    cmpwi r12, 0x1      # | If desired item variant isn't creatable
-    beq+ %end%          # |
-    li r28, 0x0         # /
+    cmpwi r12, 0x1      # | Check if desired item variant isn't creatable
+    beq+ %end%          # /
+    li r28, 0x0         # use variant 0
+    cmplwi r19, 0xFFFF         # \
+    ble+ %end%                 # | variant & 0xFFFF00FF if it's a fighter item)-
+    rlwinm r28,r19,0,24,15     # /
 }
 HOOK @ $809b0550    # itManager::getItemKindArchive
 {   
     addi r3, r28, 200       # \
     lwz	r12, 0x0014(r12)    # |
     mtctr r12               # | Original operations
-    bctrl	                # | 
-    cmpw r29, r3            # /
+    bctrl	                # / 
+    li r12, 0               # variant = 0
+    cmplwi r31, 0xFFFF      # \ check if fighter item
+    ble+ notFighterItem     # /
+    rlwinm r12,r31,0,24,15  # variant = variant & 0xFFFF00FF
+notFighterItem:             
+    cmpw r29, r3            # Original operation
 }                           
 CODE @ $809b0554    # itManager::getItemKindArchive
 {
-    blt+ -0x4C      # \
-    li r29, 0x0     # |
-    cmpwi r31, 0x0  # | If itArchive with specific variant was not found, look for itArchive with variant 0
-    li r31, 0x0     # |
+    blt+ -0x4C      # Original operation
+    li r29, 0x0     # \
+    cmpw r31, r12   # | If itArchive with specific variant was not found, look for itArchive with default variant
+    mr r31, r12     # |
     bne+ -0x5C      # /
 }
 HOOK @ $809b1cb4    # itManager::removeItemAfter
@@ -1183,15 +1198,20 @@ HOOK @ $809b1cb4    # itManager::removeItemAfter
     addi r3, r24, 200       # \
     lwz	r12, 0x0014(r12)    # |
     mtctr r12               # | Original operations
-    bctrl	                # | 
-    cmpw r23, r3            # /
+    bctrl	                # / 
+    li r12, 0               # variant = 0
+    cmplwi r29, 0xFFFF      # \ check if fighter item
+    ble+ notFighterItem     # /
+    rlwinm r12,r29,0,24,15  # variant = variant & 0xFFFF00FF
+notFighterItem:   
+    cmpw r23, r3            # Original operation
 }
 CODE @ $809b1cb8    # itManager::removeItemAfter
 {
-    blt+ -0x4C      # \
-    li r23, 0x0     # |
-    cmpwi r29, 0x0  # | If itArchive with specific variant was not found, look for itArchive with variant 0
-    li r29, 0x0     # |
+    blt+ -0x4C      # Original operation
+    li r23, 0x0     # \
+    cmpw r29, r12   # | If itArchive with specific variant was not found, look for itArchive with default variant
+    mr r29, r12     # |
     bne+ -0x5C      # /
 }
 HOOK @ $809b1e60    # itManager::removeItemAfter
@@ -1199,15 +1219,20 @@ HOOK @ $809b1e60    # itManager::removeItemAfter
     addi r3, r24, 200       # \
     lwz	r12, 0x0014(r12)    # |
     mtctr r12               # | Original operations
-    bctrl	                # | 
-    cmpw r26, r3            # /
+    bctrl	                # / 
+    li r12, 0               # variant = 0
+    cmplwi r23, 0xFFFF      # \ check if fighter item
+    ble+ notFighterItem     # /
+    rlwinm r12,r23,0,24,15  # variant = variant & 0xFFFF00FF
+notFighterItem:   
+    cmpw r26, r3            # Original operation
 }
 CODE @ $809b1e64    # itManager::removeItemAfter
 {
-    blt+ -0x4C      # \
-    li r26, 0x0     # |
-    cmpwi r23, 0x0  # | If itArchive with specific variant was not found, look for itArchive with variant 0
-    li r23, 0x0     # |
+    blt+ -0x4C      # Original operation
+    li r26, 0x0     # \
+    cmpw r23, r12   # | If itArchive with specific variant was not found, look for itArchive with default variant
+    mr r23, r12     # |
     bne+ -0x5C      # /
 }
 
@@ -3082,6 +3107,11 @@ HOOK @ $8098f6d0    # BaseItem::reset
     bne+ %end%      # / 
     li r24, 0x0
 }     
+
+######################################
+Every Item Can Have Physics [Kapedani]
+######################################
+op li r3, 0x1 @ $8098d8b8
 
 ######################################################################################
 Item Mayhem Mode (Item Switch Edition) + Butterfingers [MarioDox, DukeItOut, Kapedani]
